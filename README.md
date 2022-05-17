1bd729c1580c6297eb68fb0bc7e335f133cf9b93
Tobechukwu Alozie <tobechukwualozie@gmail.com>
2022-05-17 13:31:44 +0100

    Leads

---
+++ README.md
@@ -0,0 +1,7 @@
+<!-- To add Chrome Extension
+
+1. Go to chrome://extensions
+2. Trigger developer mode
+3. Click "Load unpacked"
+4. Select the leads file downloaded
+Navigate to a new page and click on extensions, the leads tracker will be there. -->
---
+++ scripts.js
@@ -0,0 +1,86 @@
+const inputed = document.getElementById("theLeads");
+let listed = document.getElementById("listLeads");
+let theLeads = [];
+
+if (localStorage.getItem("theLeads") === null) {
+    theLeads = [];
+    let eachLead = "";
+} else {
+    let eachLead = "";
+    theLeads = JSON.parse(localStorage.getItem("theLeads"));
+    printLead(theLeads);
+}
+
+document.getElementById("saveLeads").onclick = saveLeades;
+document.getElementById("deleteLeads").onclick = deleteLead;
+document.getElementById("closePage").onclick = closedPage;
+document.getElementById("saveTab").onclick = saveCurrentTab;
+
+
+
+let x = "";
+
+
+function closedPage() {
+    window.close();
+}
+
+function printLead(some) {
+    eachLead = "";
+    for (let i = 0; i < some.length; i++) {
+      
+        eachLead += `<li> <a target='_blank' href='${some[i]}'>${some[i]}</a></li>`;
+        console.log(eachLead);
+
+    }
+  
+    listed.innerHTML = eachLead;
+}
+
+
+
+function saveLeades() {
+    
+    x = inputed.value;
+    inputed.value = "";
+    theLeads.push(x);
+    // console.log(theLeads);
+    printLead(theLeads);
+    localStorage.setItem("theLeads", JSON.stringify(theLeads));
+    
+
+}
+
+function deleteLead() {
+    localStorage.clear();
+    document.getElementById("listLeads").innerHTML = "";
+    theLeads = [];
+    printLead(theLeads);
+}
+
+function saveCurrentTab() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        theLeads.push(tabs[0].url);
+        localStorage.setItem("theLeads", JSON.stringify(theLeads));
+        printLead(theLeads);
+
+    })
+
+    
+}
+
+function generateSentence(desc, arr) {
+    var x = arr.length;
+    let froots = "";
+    for (let i = 0; i < arr.length; i++) {
+        if (i < arr.length - 2) {
+            froots += arr[i] + ", ";
+        } else {
+            froots += arr[i];
+        }
+    }
+
+
+
+    return `The ${x} ${desc} are ${froots}.`
+}
